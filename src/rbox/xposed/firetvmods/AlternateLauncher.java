7d104ba46669bb888fa2213e6b80e09f92e004c8
nikk gitanes <tsynik@gmail.com>
2019-10-31 10:17:25 +0300

    FireOS 6 changes

--- src/rbox/xposed/firetvmods/AlternateLauncher.java
+++ src/rbox/xposed/firetvmods/AlternateLauncher.java
@@ -3,18 +3,41 @@ package rbox.xposed.firetvmods;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 
 import java.util.List;
-
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.util.Log;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+import java.util.Set;
 
 public class AlternateLauncher implements IXposedHookLoadPackage
 {
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable
 	{
+
+		// hook KFTV Launcher
+//		if (lpparam.packageName.equals("com.amazon.tv.launcher"))
+//		{
+//			Log.i("### IN ### ", "com.amazon.tv.launcher");
+//			findAndHookMethod("com.amazon.tv.launcher.ui.HomeActivity_vNext", lpparam.classLoader, "onNewIntent", Intent.class, new XC_MethodHook() {
+//				@Override
+//				protected void afterHookedMethod(MethodHookParam param) throws Throwable
+//				{
+//					Log.i("### onNewIntent ### ", "com.amazon.tv.launcher.ui.HomeActivity_vNext");
+//					Intent intent = (Intent)param.args[0];
+//					Bundle extras = intent.getExtras();
+//					if (extras != null) {
+//						Log.i("### has extras ### ", extras.toString());
+//					}
+//				}
+//			});
+//		}
+
 		if (!lpparam.packageName.equals("android"))
 			return;
 
@@ -25,14 +48,46 @@ public class AlternateLauncher implements IXposedHookLoadPackage
 			{
 				@SuppressWarnings("unchecked")
 				List<ResolveInfo> query = (List<ResolveInfo>)param.args[3];
+				Intent intent = (Intent)param.args[0];
+				Set<String> categories = intent.getCategories();
+				Bundle extras = intent.getExtras();
+				boolean loadSettings = false;
+				// DEBUG FireOS 6.2.6.6
+				// 0 ### : com.amazon.tv.launcher.ui.HomeActivity_vNext priority: 950
+				// 1 ### : com.amazon.firehomestarter.HomeStarterActivity priority: 1
+				// 2 ### : com.amazon.tv.leanbacklauncher.MainActivity priority: 0
+				// 3 ### : com.amazon.tv.settings.v2.system.FallbackHome priority: -1000
+//				Log.d("### I ### ", param.args[0].toString());
+//				if (extras != null) {
+//					for (String key : extras.keySet()) {
+//						Log.d("### E ### ", key + " : " + (extras.get(key) != null ? extras.get(key) : "NULL"));
+//					}
+//				}
+//				for (int i=0; i < query.size(); i++) {
+//					Log.d("### " + i + " ### ", query.get(i).activityInfo.name + " priority: " + query.get(i).priority);
+//				}
 
-				// If there are at least 2 activities and this is Amazon home activity,
-				// swap them so the 2nd one is used
-				if (query.size() >= 2 && "com.amazon.tv.launcher.ui.HomeActivity".equals(query.get(0).activityInfo.name))
-				{
-					ResolveInfo secondLauncher = query.get(1);
-					query.set(1, query.get(0));
-					query.set(0, secondLauncher);
+				if (Intent.ACTION_MAIN.equals(intent.getAction())
+					&& categories != null
+					&& categories.size() == 1
+					&& categories.contains(Intent.CATEGORY_HOME)) {
+
+					// Check if we load Settings
+					if (extras != null && extras.containsKey("navigate_node") && extras.get("navigate_node").equals("l_settings")) {
+						loadSettings = true;
+					}
+					// If there are at least 3 activities and 1st one is Amazon Launcher
+					// swap them so the 3rd one is used (don't swap with FallbackHome)
+					if (query.size() > 2
+						&& query.get(0).activityInfo.name.contains("com.amazon.tv.launcher.ui.HomeActivity")
+						&& query.get(2).priority == 0
+						&& !query.get(2).activityInfo.name.contains("com.amazon.tv.settings.v2.system.FallbackHome")
+						&& !loadSettings)
+					{
+						ResolveInfo thirdLauncher = query.get(2);
+						query.set(2, query.get(0));
+						query.set(0, thirdLauncher);
+					}
 				}
 			}
 		});
--- src/rbox/xposed/firetvmods/KeyBindings.java
+++ src/rbox/xposed/firetvmods/KeyBindings.java
@@ -4,7 +4,10 @@ import java.io.File;
 import java.util.Map.Entry;
 
 import android.content.Context;
+import android.os.Build;
+import android.util.Log;
 import android.util.SparseArray;
+import android.view.inputmethod.InputMethodManager;
 import android.view.KeyEvent;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
@@ -17,52 +20,82 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 public class KeyBindings implements IXposedHookZygoteInit, IXposedHookLoadPackage
 {
 	private static final int LONG_PRESS = 0x80000000;
+	private static final String TAG = "KeyBindings";
+	private static String PhoneWindowMgr;
 	SparseArray<String> bindings;
 
 	@Override
 	public void initZygote(StartupParam startupParam) throws Throwable
 	{
 		bindings = new SparseArray<String>();
-
+		Log.d(TAG, "initZygote");
 		// Add the home long press to recents by default
 		bindings.put(LONG_PRESS | KeyEvent.KEYCODE_HOME, null);
+		// SEARCH
+		bindings.put(0 | KeyEvent.KEYCODE_SEARCH, "com.google.android.katniss");
 
-		XSharedPreferences prefs = new XSharedPreferences(new File("/data/media/0/key_bindings.xml"));
-		for (Entry<String, ?> e : prefs.getAll().entrySet())
-		{
-			String key = e.getKey();
-			int longPress = 0;
-			if (key.endsWith("_LONG"))
-			{
-				longPress = LONG_PRESS;
-				key = key.substring(0, key.indexOf("_LONG"));
-			}
-			bindings.put(longPress | XposedHelpers.getStaticIntField(KeyEvent.class, key), (String)e.getValue());
-		}
+		// FIXME: sdcard permissions
+//		XSharedPreferences prefs = new XSharedPreferences(new File("/data/media/0/key_bindings.xml"));
+//		prefs.makeWorldReadable();
+//		for (Entry<String, ?> e : prefs.getAll().entrySet())
+//		{
+//			String key = e.getKey();
+//			int longPress = 0;
+//			if (key.endsWith("_LONG"))
+//			{
+//				longPress = LONG_PRESS;
+//				key = key.substring(0, key.indexOf("_LONG"));
+//			}
+//			bindings.put(longPress | XposedHelpers.getStaticIntField(KeyEvent.class, key), (String)e.getValue());
+//		}
 	}
 
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable
 	{
+		// DEBUG
+		StringBuilder stringBuilder = new StringBuilder();
+		int size = bindings.size();
+		stringBuilder.append("{ ");
+		for (int i = 0; i < size; i++) {
+			stringBuilder.append(bindings.keyAt(i)).append(" = ")
+			   .append(bindings.valueAt(i));
+			if (i < (size - 1)) {
+				stringBuilder.append(", ");
+			}
+		}
+		stringBuilder.append(" }");
+		Log.d(TAG, "### bindings ### " + stringBuilder.toString());
+
 		// Don't do the hook if the prefs were empty
 		if (!lpparam.packageName.equals("android") || bindings.size() == 0)
 			return;
 
+		if (Build.VERSION.SDK_INT >= 23) // M
+			PhoneWindowMgr = "com.android.server.policy.PhoneWindowManager";
+		else
+			PhoneWindowMgr = "com.android.internal.policy.impl.PhoneWindowManager";
+
 		// Add a hook for setting up recents on menu long press
 		if (bindings.get(LONG_PRESS | KeyEvent.KEYCODE_HOME) == null)
 		{
-			XposedHelpers.findAndHookConstructor("com.android.internal.policy.impl.PhoneWindowManager", lpparam.classLoader, new XC_MethodHook() {
+			XposedHelpers.findAndHookConstructor(PhoneWindowMgr, lpparam.classLoader, new XC_MethodHook() {
 				@Override
 				protected void afterHookedMethod(MethodHookParam param) throws Throwable
 				{
 					// Set the long press behavior to bring up the Recents dialog
-					XposedHelpers.setIntField(param.thisObject, "mLongPressOnHomeBehavior", 1);
+					// static final int LONG_PRESS_HOME_NOTHING = 0;
+					// static final int LONG_PRESS_HOME_RECENT_DIALOG = 1;
+					// static final int LONG_PRESS_HOME_RECENT_SYSTEM_UI = 2;
+					// static final int LONG_PRESS_HOME_VOICE_SEARCH = 3;
+					Log.d(TAG, "### mLongPressOnHomeBehavior ### 0 ### ");
+					XposedHelpers.setIntField(param.thisObject, "mLongPressOnHomeBehavior", 2);
 				}
 			});
 		}
 
 		// For some reason, findAndHookMethod doesn't work for this
-		XposedBridge.hookAllMethods(XposedHelpers.findClass("com.android.internal.policy.impl.PhoneWindowManager", lpparam.classLoader), "interceptKeyBeforeDispatching", new XC_MethodHook() {
+		XposedBridge.hookAllMethods(XposedHelpers.findClass(PhoneWindowMgr, lpparam.classLoader), "interceptKeyBeforeDispatching", new XC_MethodHook() {
 			@Override
 			protected void beforeHookedMethod(MethodHookParam param) throws Throwable
 			{
@@ -70,12 +103,21 @@ public class KeyBindings implements IXposedHookZygoteInit, IXposedHookLoadPackag
 				if (event.getAction() == KeyEvent.ACTION_DOWN)
 				{
 					int longPress = (event.getFlags() & KeyEvent.FLAG_LONG_PRESS) != 0 ? LONG_PRESS : 0;
+					// if (longPress != 0)
+					//	Log.d(TAG, "### long press ### " + longPress);
 					String value = bindings.get(longPress | event.getKeyCode());
 					if (value != null)
 					{
+						Log.d(TAG, " ### start ### " + value);
 						Context mContext = (Context)XposedHelpers.getObjectField(param.thisObject, "mContext");
 						mContext.startActivity(mContext.getPackageManager().getLaunchIntentForPackage(value));
 						param.setResult(-1);
+					}
+					// LONG PRESSED MENU
+					if (longPress != 0 && event.getKeyCode() == KeyEvent.KEYCODE_MENU) {
+						Log.d(TAG, " ### MENU_LONG ### ");
+						Context mContext = (Context)XposedHelpers.getObjectField(param.thisObject, "mContext");
+						((InputMethodManager) mContext.getSystemService("input_method")).showInputMethodPicker();
 					}
 				}
 			}
