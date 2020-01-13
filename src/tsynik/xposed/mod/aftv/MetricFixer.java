10170a968242db7bd66dac4820ad44a53a0e7efc
nikk gitanes <tsynik@gmail.com>
2020-01-13 11:02:01 +0300

    sync metrics code (not used)

--- src/tsynik/xposed/mod/aftv/MetricFixer.java
+++ src/tsynik/xposed/mod/aftv/MetricFixer.java
@@ -1,5 +1,6 @@
 package tsynik.xposed.mod.aftv;
 
+import android.content.Context;
 import android.util.Log;
 
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
@@ -7,6 +8,8 @@ import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
+
+import java.util.List;
 
 import tsynik.xposed.mod.aftv.BuildConfig;
 
@@ -17,165 +20,109 @@ public class MetricFixer implements IXposedHookLoadPackage
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable
 	{
-
-		// if (BuildConfig.DEBUG) Log.i(TAG, "### packageName ### " + lpparam.packageName);
-		Class<?> MetricsServiceConnection = XposedHelpers.findClass("com.amazon.client.metrics.MetricsServiceConnection", lpparam.classLoader);
-		XposedHelpers.findAndHookMethod(MetricsServiceConnection, "getService", new XC_MethodHook() {
-			@Override
-			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-			    //if (BuildConfig.DEBUG) Log.i(TAG, "### MetricsApi ### override getService to null");
-		        param.setResult(null);
-		    }
-		});
-
-		Class<?> AndroidMetricsFactoryImpl = XposedHelpers.findClass("com.amazon.client.metrics.AndroidMetricsFactoryImpl", lpparam.classLoader);
-		XposedHelpers.findAndHookMethod(AndroidMetricsFactoryImpl, "shouldRecordMetrics", new XC_MethodHook() {
-			@Override
-			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-			    //if (BuildConfig.DEBUG) Log.i(TAG, "### MetricsApi ### override shouldRecordMetrics to false");
-		        param.setResult(false);
-		    }
-		});
-
-//		// DeviceControl hook
-//		if (lpparam.packageName.equals("com.amazon.tv.devicecontrol"))
-//		{
-//			XposedBridge.log("Loaded app: " + lpparam.packageName);
-//			Class<?> Metric = XposedHelpers.findClass("com.amazon.tv.oz.metrics.Metric", lpparam.classLoader);
-//			Class<?> DeviceControlApiService = XposedHelpers.findClass("com.amazon.tv.devicecontrol.api.DeviceControlApiService", lpparam.classLoader);
-//			Class<?> AsyncMetricsRecorder = XposedHelpers.findClass("com.amazon.tv.oz.metrics.engine.pmet.recorder.AsyncMetricsRecorder", lpparam.classLoader);
-//			Class<?> DcmMetricsRecorder = XposedHelpers.findClass("com.amazon.tv.oz.metrics.engine.pmet.recorder.DcmMetricsRecorder", lpparam.classLoader);
-//			Class<?> MetricsRecord = XposedHelpers.findClass("com.amazon.tv.oz.metrics.engine.pmet.MetricsRecord", lpparam.classLoader);
-//			XposedHelpers.findAndHookMethod(DeviceControlApiService, "trackMetrics", Metric, new XC_MethodHook() {
-//				@Override
-//				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-//					if (BuildConfig.DEBUG) Log.i(TAG, "### DeviceControl ### override trackMetrics to null");
-//					param.setResult(null); // NULL method
+		if (!lpparam.packageName.equals("android") &&
+			lpparam.packageName.contains("com.amazon")) {
+			// if (BuildConfig.DEBUG) Log.i(TAG, " ### IN ### called for package: " + lpparam.packageName);
+			Class<?> MetricsServiceConnection = XposedHelpers.findClassIfExists("com.amazon.client.metrics.MetricsServiceConnection", lpparam.classLoader);
+			if (MetricsServiceConnection != null) XposedHelpers.findAndHookMethod(MetricsServiceConnection, "getService", new XC_MethodHook() {
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+					// if (BuildConfig.DEBUG) Log.i(TAG, "### MetricsApi ### override getService to null in " + lpparam.packageName);
+					param.setResult(null);
+				}
+			});
+			Class<?> AndroidMetricsFactoryImpl = XposedHelpers.findClassIfExists("com.amazon.client.metrics.AndroidMetricsFactoryImpl", lpparam.classLoader);
+			if (AndroidMetricsFactoryImpl != null) XposedHelpers.findAndHookMethod(AndroidMetricsFactoryImpl, "shouldRecordMetrics", new XC_MethodHook() {
+//				protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+//					if (BuildConfig.DEBUG) Log.d(TAG, "### AndroidMetricsFactoryImpl ### replace shouldRecordMetrics() false in " + lpparam.packageName);
+//					return false;
 //				}
-//			});
-//			XposedHelpers.findAndHookMethod(AsyncMetricsRecorder, "record", MetricsRecord, new XC_MethodHook() {
-//				@Override
-//				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-//					if (BuildConfig.DEBUG) Log.i(TAG, "### DeviceControl ### override AsyncMetricsRecorder record to null");
-//					param.setResult(null); // NULL method
-//				}
-//			});
-//			XposedHelpers.findAndHookMethod(DcmMetricsRecorder, "record", MetricsRecord, new XC_MethodHook() {
-//				@Override
-//				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-//					if (BuildConfig.DEBUG) Log.i(TAG, "### DeviceControl ### override DcmMetricsRecorder record to null");
-//					param.setResult(null); // NULL method
-//				}
-//			});
-//		}
-
-//		// Amazon Client Metrics API hook
-//		if (lpparam.packageName.equals("com.amazon.client.metrics.api"))
-//		{
-//			XposedBridge.log("Loaded app: " + lpparam.packageName);
-//			Class<?> AndroidMetricsFactoryImpl = XposedHelpers.findClass("com.amazon.client.metrics.AndroidMetricsFactoryImpl", lpparam.classLoader);
-//			//Class<?> MetricsServiceConnection = XposedHelpers.findClass("com.amazon.client.metrics.MetricsServiceConnection", lpparam.classLoader);
-//			Class<?> AbstractMetricsFactoryImpl = XposedHelpers.findClass("com.amazon.client.metrics.AbstractMetricsFactoryImpl", lpparam.classLoader);
-//			Class<?> BaseMetricsFactoryImpl = XposedHelpers.findClass("com.amazon.client.metrics.BaseMetricsFactoryImpl", lpparam.classLoader);
-//			Class<?> MetricEvent = XposedHelpers.findClass("com.amazon.client.metrics.MetricEvent", lpparam.classLoader);
-//			Class<?> Priority = XposedHelpers.findClass("com.amazon.client.metrics.Priority", lpparam.classLoader);
-//			Class<?> Channel = XposedHelpers.findClass("com.amazon.client.metrics.Channel", lpparam.classLoader);
-//			Class<?> MetricsServiceWrapper = XposedHelpers.findClass("com.amazon.client.metrics.MetricsServiceWrapper", lpparam.classLoader);
-//
-//			XposedHelpers.findAndHookMethod(MetricsServiceWrapper, "getBoundService", new XC_MethodHook() {
-//			    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-//			    	if (BuildConfig.DEBUG) Log.i(TAG, "### MetricsApi ### override getBoundService to null");
-//		        	return null;
-//		        }
-//			});
-//
-//			XposedHelpers.findAndHookMethod(AndroidMetricsFactoryImpl, "getInstance", new XC_MethodHook() {
-//			    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-//			    	if (BuildConfig.DEBUG) Log.i(TAG, "### MetricsApi ### override getInstance to null");
-//		        	return null;
-//		        }
-//			});
-//			XposedHelpers.findAndHookMethod(AndroidMetricsFactoryImpl, "getInstance", Context.class, new XC_MethodHook() {
-//			    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-//			    	if (BuildConfig.DEBUG) Log.i(TAG, "### MetricsApi ### override getInstance to null");
-//		        	return null;
-//		        }
-//			});
-//
-//			XposedHelpers.findAndHookMethod(AbstractMetricsFactoryImpl, "record", MetricEvent, Priority, Channel, new XC_MethodHook() {
-//			//	@Override
-//			//	protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-//			//		// pass null event
-//			//		param.args[0] = null;
-//			//	}
-//			    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-//		        	return null;
-//		        }
-//			});
-//			// BaseMetricsFactoryImpl
-//			Class<?> MetricEventType = XposedHelpers.findClass("com.amazon.client.metrics.MetricEventType", lpparam.classLoader);
-//			XposedHelpers.findAndHookMethod(BaseMetricsFactoryImpl, "createMetricEvent", String.class, String.class, new XC_MethodHook() {
-//			    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-//		        	return null;
-//		        }
-//			});
-//			XposedHelpers.findAndHookMethod(BaseMetricsFactoryImpl, "createMetricEvent", String.class, String.class, MetricEventType, new XC_MethodHook() {
-//			    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-//		        	return null;
-//		        }
-//			});
-//			XposedHelpers.findAndHookMethod(BaseMetricsFactoryImpl, "createMetricEvent", String.class, String.class, MetricEventType, Boolean.class, new XC_MethodHook() {
-//			    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-//		        	return null;
-//		        }
-//			});
-//			XposedHelpers.findAndHookMethod(BaseMetricsFactoryImpl, "createConcurrentMetricEvent", String.class, String.class, new XC_MethodHook() {
-//			    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-//		        	return null;
-//		        }
-//			});
-//			XposedHelpers.findAndHookMethod(BaseMetricsFactoryImpl, "createConcurrentMetricEvent", String.class, String.class, MetricEventType, new XC_MethodHook() {
-//			    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-//		        	return null;
-//		        }
-//			});
-//			XposedHelpers.findAndHookMethod(BaseMetricsFactoryImpl, "createConcurrentMetricEvent", String.class, String.class, MetricEventType, Boolean.class, new XC_MethodHook() {
-//			    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-//		        	return null;
-//		        }
-//			});
-//			XposedHelpers.findAndHookMethod(BaseMetricsFactoryImpl, "shouldRecordMetrics", new XC_MethodHook() {
-//			    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-//		        	// return false;
-//		        	return Boolean.valueOf(false);
-//		        }
-//			});
-//			XposedHelpers.findAndHookMethod(BaseMetricsFactoryImpl, "record", MetricEvent, new XC_MethodHook() {
-//			    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-//		        	return null;
-//		        }
-//			});
-//			XposedHelpers.findAndHookMethod(BaseMetricsFactoryImpl, "record", MetricEvent, Priority, new XC_MethodHook() {
-//			    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-//		        	return null;
-//		        }
-//			});
-//
-//		}
-
-//		// Amazon Client Metrics hook
-//		if (lpparam.packageName.equals("com.amazon.client.metrics"))
-//		{
-//			Class<?> AndroidMetricsServiceAdapter = XposedHelpers.findClass("com.amazon.client.metrics.AndroidMetricsServiceAdapter", lpparam.classLoader);
-//			XposedHelpers.findAndHookMethod(AndroidMetricsServiceAdapter, "getRecordMetricsSetting", new XC_MethodHook() {
-//				@Override
-//				protected void afterHookedMethod(MethodHookParam param) throws Throwable
-//				{
-//					if (BuildConfig.DEBUG) Log.i(TAG, "### com.amazon.client.metrics ### AndroidMetricsFactoryImpl ### override getRecordMetricsSetting to false");
-//					param.setResult(false); // return false
-//				}
-//			});
-//		}
-
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+					if (BuildConfig.DEBUG) Log.i(TAG, "### MetricsApi ### override shouldRecordMetrics to false");
+					param.setResult(false);
+				}
+			});
+			// MetricsBlacklist
+			Class<?> MetricsBlacklist = XposedHelpers.findClassIfExists("com.amazon.client.metrics.MetricsBlacklist", lpparam.classLoader);
+			if (MetricsBlacklist != null) XposedHelpers.findAndHookMethod(MetricsBlacklist, "isBlacklisted", String.class, String.class, new XC_MethodHook() {
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+					// if (BuildConfig.DEBUG) Log.d(TAG, "### MetricsBlacklist ### override isBlacklisted to true in " + lpparam.packageName);
+					param.setResult(true);
+				}
+			});
+			// MetricsServiceWrapper
+			Class<?> MetricsServiceWrapper = XposedHelpers.findClassIfExists("com.amazon.client.metrics.MetricsServiceWrapper", lpparam.classLoader);
+			if (MetricsServiceWrapper != null) {
+				XposedHelpers.findAndHookMethod(MetricsServiceWrapper, "getRecordMetricsSetting", new XC_MethodHook() {
+					@Override
+					protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+						if (BuildConfig.DEBUG) Log.d(TAG, "### MetricsServiceWrapper ### override getRecordMetricsSetting to false in " + lpparam.packageName);
+						param.setResult(false);
+					}
+				});
+				// void record(int priority, String program, String source, long timestamp, List<DataPointEnvelope> datapoints)
+				XposedHelpers.findAndHookMethod(MetricsServiceWrapper, "record", int.class, String.class, String.class, long.class, List.class, new XC_MethodHook() {
+					@Override
+					protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+						if (BuildConfig.DEBUG) Log.i(TAG, "### MetricsServiceWrapper ### override record in " + lpparam.packageName);
+						param.setResult(null);
+					}
+				});
+			}
+		}
+		// KFTV Launcher
+		if (lpparam.packageName.equals("com.amazon.tv.launcher")) {
+			// ERR: AndroidRuntime:
+			// java.lang.IncompatibleClassChangeError:
+			// Class 'com.amazon.client.metrics.NullClickStreamMetricEvent' does not implement
+			// interface 'java.lang.CharSequence' in call to 'int java.lang.CharSequence.length()'
+			// AndroidRuntime:
+			// at android.text.TextUtils.isEmpty()
+			// at com.amazon.tv.metrics.LauncherSessionIdManager.getSessionId()
+			Class<?> AndroidMetricsFactoryImpl = XposedHelpers.findClassIfExists("com.amazon.tv.metrics.LauncherSessionIdManager", lpparam.classLoader);
+			if (AndroidMetricsFactoryImpl != null) XposedHelpers.findAndHookMethod(AndroidMetricsFactoryImpl, "getSessionId", new XC_MethodHook() {
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+					//if (BuildConfig.DEBUG) Log.i(TAG, "### MetricsApi ### fix null SessionID in " + lpparam.packageName);
+					param.setResult(" "); // null SessionID
+				}
+			});
+		}
+		// TTS Lib
+		if (lpparam.packageName.contains("com.ivona.tts")) {
+			Class<?> AmazonMetrics = XposedHelpers.findClassIfExists("com.ivona.ttslib.metrics.a", lpparam.classLoader);
+			if (AmazonMetrics != null) {
+				Class<?> IvonaMetricEvent = XposedHelpers.findClassIfExists("com.ivona.ttslib.metrics.IvonaMetricEvent", lpparam.classLoader);
+				if (IvonaMetricEvent != null) XposedHelpers.findAndHookMethod(AmazonMetrics, "a", IvonaMetricEvent, new XC_MethodHook() {
+						@Override
+						protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+							// if (BuildConfig.DEBUG) Log.d(TAG, "### VoiceApp ### NULL a(IvonaMetricEvent) in " + lpparam.packageName);
+							param.setResult(null);
+						}
+				});
+			}
+		}
+		// AVOD
+		if (lpparam.packageName.equals("com.amazon.avod")) {
+			Class<?> DcmConfiguration = XposedHelpers.findClassIfExists("com.amazon.avod.metrics.DcmConfiguration", lpparam.classLoader);
+			if (DcmConfiguration != null) {
+				XposedHelpers.findAndHookMethod(DcmConfiguration, "shouldRecordMetricsForDefaultProgramName", new XC_MethodHook() {
+					@Override
+					protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+						if (BuildConfig.DEBUG) Log.d(TAG, "### avod ### shouldRecordMetricsForDefaultProgramName false in " + lpparam.packageName);
+						param.setResult(false);
+					}
+				});
+				XposedHelpers.findAndHookMethod(DcmConfiguration, "shouldRecordMetricsForAlternateProgramName", new XC_MethodHook() {
+					@Override
+					protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+						if (BuildConfig.DEBUG) Log.d(TAG, "### avod ### shouldRecordMetricsForAlternateProgramName false in " + lpparam.packageName);
+						param.setResult(false);
+					}
+				});
+			}
+		}
 	}
 }
